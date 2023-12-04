df8584abe9207e7309ca7f3dd99fd5bd476caaf1
Burns <nicholas.j.burns@vanderbilt.edu>
2023-12-04 17:11:46 -0600

    Documented clustering test files

--- hierarchical_test.py
+++ hierarchical_test.py
@@ -1,28 +1,49 @@
+# Import necessary libraries
 import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_score
 
+# Read data from CSV file into a Pandas DataFrame
 df = pd.read_csv('data_files/new_data.csv')
+
+# Transpose the DataFrame to get features as rows
 features = df.iloc[:, 1:].transpose().values.tolist()
 
+# Extract the features as a list of tuples
 data = list(zip(*features))
 
+# Initialize variables to track the best clustering results
 max_silhouette_avg = 0
 best_n_clusters = 0
 labels = []
+
+# Iterate over a range of cluster numbers from 2 to 899
 for i in range(2, 900):
+    # Create an Agglomerative Clustering model with current cluster number
     hierarchical_cluster = AgglomerativeClustering(n_clusters=i, affinity='euclidean', linkage='ward')
+
+    # Fit the model and predict cluster labels
     cluster_labels = hierarchical_cluster.fit_predict(data)
+
+    # Calculate the silhouette score for the current clustering
     silhouette_avg = silhouette_score(data, cluster_labels)
-    print("For no of clusters = ", i,
-          " The average silhouette_score is: ", silhouette_avg)
+
+    # Print silhouette score for current cluster number
+    print("For no of clusters = ", i, " The average silhouette_score is: ", silhouette_avg)
+
+    # Update best clustering if the current silhouette score is higher
     if silhouette_avg > max_silhouette_avg:
         max_silhouette_avg = silhouette_avg
         best_n_clusters = i
         labels = cluster_labels
+
+# Print the final cluster labels and best clustering results
 print(labels)
 print("Clusters: ", best_n_clusters)
 print("Silhouette score: ", max_silhouette_avg)
 
+# Replace the column 'utility' of the original DataFrame with cluster labels
 df['utility'] = labels
+
+# Save the clustered data to a new CSV file
 df.to_csv('data_files/clustered_data.csv', encoding='utf-8', index=False)
--- kmeans_test.py
+++ kmeans_test.py
@@ -1,24 +1,41 @@
+# Import necessary libraries
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
+# Read data from CSV file into a Pandas DataFrame
 df = pd.read_csv('data_files/new_data.csv')
 
+# Extract numerical data from DataFrame and convert it to a list
 data = df.iloc[:, 1:].values.tolist()
 
+# Initialize variables to track the best clustering parameters and results
 max_silhouette_avg = 0
 best_n_clusters = 0
 labels = []
+
+# Iterate through a range of cluster numbers to find the optimal number of clusters
 for i in range(2, 900):
+    # Create a KMeans clustering model with the current number of clusters
     kmeans = KMeans(n_clusters=i, random_state=2)
+
+    # Fit the model to the data and predict cluster labels
     cluster_labels = kmeans.fit_predict(data)
+
+    # Calculate the silhouette score for the current clustering
     silhouette_avg = silhouette_score(data, cluster_labels)
-    print("For no of clusters = ", i,
-          " The average silhouette_score is: ", silhouette_avg)
+
+    # Print the silhouette score for the current iteration
+    print("For no of clusters =", i,
+          "The average silhouette_score is:", silhouette_avg)
+
+    # Update the best clustering parameters if the current result is better
     if silhouette_avg > max_silhouette_avg:
         max_silhouette_avg = silhouette_avg
         best_n_clusters = i
         labels = cluster_labels
+
+# Print the final cluster labels and the best clustering parameters
 print(labels)
-print("Clusters: ", best_n_clusters)
-print("Silhouette score: ", max_silhouette_avg)
+print("Clusters:", best_n_clusters)
+print("Silhouette score:", max_silhouette_avg)
